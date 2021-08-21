e71f7f811f8bdb231257c5f626358011068d1043
Chandan Kumar <ck220698@gmail.com>
2021-08-21 19:59:23 +0530

    Add files via upload

---
+++ Python/Python Classroom Session/Python Session - 1.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Comments
+
+
+# In[2]:
+
+
+"Hello World"
+
+
+# In[4]:
+
+
+print("Hello World")
+
+
+# - Identifier
+
+# In[6]:
+
+
+a = 10     ## a is a variable, Storing 10 in memory
+
+
+# In[7]:
+
+
+print(a)
+
+
+# In[10]:
+
+
+import keyword
+
+
+# In[11]:
+
+
+keyword.kwlist
+
+
+# In[12]:
+
+
+len(keyword.kwlist)  ##Len is inbuilt function which returns the length 
+
+
+# In[16]:
+
+
+a = "True"
+b = True
+print(type(a))
+print(type(b))
+
+
+# - DataTypes
+
+# In[17]:
+
+
+'''
+Data Types:
+1. Decimal (Integer) (Types of Decimal - Binary, Decimal, Octal, Hexadecimal)
+2. Float
+3. Boolean
+4. Bytes
+5. Bytes Array
+6. String
+7. Complex
+'''
+
+'''
+Data Structures
+1. List
+2. Tuples
+3. Dictionary
+4. Set
+5. Frozen Set
+6. Range
+7. None
+8. Arrays
+'''
+
+
+# In[33]:
+
+
+a = 13        # Decimal value
+bin(a)        # Result in binary format
+
+
+# In[34]:
+
+
+a1 = 0b1101   # Binary value
+print(a1)     # Binary to Decimal
+
+
+# In[43]:
+
+
+b1 = 0x69FE   ## Hex Value
+print(b1)     ## Hexa to decimal
+
+
+# In[44]:
+
+
+b = 27134      ## Decimal Value
+hex(b)        ## Result in hexa format
+
+
+# In[38]:
+
+
+d1 = 21312  ## Decimal value
+oct(d1)     ## Printing in Octal formal
+
+
+# In[40]:
+
+
+d = 0o51500  ## Octal Value
+print(d)    ## Octal to decimal
+
+
+# In[ ]:
+
+
+
+
